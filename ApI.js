f8be89ebafb3fb2e005b4c46e5a024e78cb1a267
Nitinpatel2532 <nitinpatel74084@gmail.com>
2025-01-27 19:44:16 +0530

    success

---
+++ ApI.js
@@ -0,0 +1,33 @@
+// Api -> application proframming interface...........
+//  -> contain predefined data / modules -> used in programms..
+
+// Fetch ()  -> method / function   -> return promises object
+
+// Http request
+// Method ->
+// get -> fetch the data
+// post -> insert data into api
+// delete -> delete the data
+// put -> update the data
+
+// promise -> object
+// 3 stage of promises...........
+// 1. pending
+// 2. fullfill
+// 3. reject
+
+// handle promises
+// 1. async
+// 2. await
+
+// syncronous ->  execute line by line execution
+// asynchronous  -> execution the line which take less time
+
+// javascirpt is a synchronous language due to its a lightweight its work so fast.
+
+console.log("fetch api")
+
+
+
+
+
