c3fea03ddeb8c1d45f791a8f934e32a0c3212ba7
Nitinpatel2532 <nitinpatel74084@gmail.com>
2025-01-29 05:05:20 +0530

    Success

--- ApI.js
+++ ApI.js
@@ -20,13 +20,29 @@
 // 1. async
 // 2. await
 
-// syncronous ->  execute line by line execution
+// syncronous ->  execute line by line execution 
 // asynchronous  -> execution the line which take less time
 
 // javascirpt is a synchronous language due to its a lightweight its work so fast.
 
 console.log("fetch api")
 
+// node.js  ->LTS-> long term support
+// runtime environment
+
+// fetch() ->pass two arguments  ->to fetch the api data
+// url , object form -> (method)
+
+// JSON placeholder to take fake API for testing purpose........
+
+
+async function fetch_data() {
+    let data = await fetch("https://jsonplaceholder.typicode.com/posts")
+    let res=data.json()
+    console.log(res)
+}
+
+fetch_data();
 
 
 
